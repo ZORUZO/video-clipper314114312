e6eea029f1437dc0bdec52b4240de93d6e5475f6
ZORUZO <109348068+ZORUZO@users.noreply.github.com>
2024-07-06 16:52:09 +0900

    recommit

--- app.js
+++ app.js
@@ -21,13 +21,14 @@ app.get("/", async (req, res, next) => {
 });
 const allowedFileTypes = ['video/mp4','audio/wav'];
 const fileFilter = (req, file, cb) => {
-  if (allowedFileTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+        if (allowedFileTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+        cb(null, false);
+        }
+  
+ 
 };
-
 const  storage  =  multer.diskStorage({ 
     destination: function (req, file, cb) {
         cb(null, 'tmp/')
@@ -43,17 +44,41 @@ const  storage  =  multer.diskStorage({
         }
       }
   })
-const upload = multer({ fileFilter:fileFilter,storage:storage, limits: { fileSize: 50 * 1024 * 1024 }, // 2MB
-},
-);
+const upload = multer({ fileFilter:fileFilter,
+    storage:storage,
+    limits: { fileSize: 50 * 1024 * 1024 } // 50MB
+});
 
 
 app.post("/download",upload.single('file'),(req,res,next)=>{
-  
-    try {
         var starttime= req.body['starttime'];
         var duration = req.body['endtime']-starttime;
-        if(req.file.mimetype=='video/mp4'){
+        if(duration<=0){
+            res.send("切り抜き開始秒数が、切り抜き終了秒数よりも小さくなっています");
+        }
+        try{
+            if(req.file.mimetype!=='video/mp4' && req.file.mimetype!=='audio/wav'){
+                res.send('mp4か、wavファイルを選択してください')  
+            }
+        } catch(err){
+            console.log(req.file.mimetype)
+            res.send('mp4か、wavファイルを選択してください')
+        }
+
+        if (req.file.mimetype==='audio/wav'){
+            async function videoTrimming(starttime,duration){
+                //ffmpegを使い、音声ファイルを切り抜き
+                await childProcess.execSync(`ffmpeg -nostdin -ss ${starttime} -i tmp/tmp.wav -t ${duration} -c copy tmp/output.wav`);
+                res.download("tmp/output.wav",(err)=>{
+                    //使い終わった音声ファイルを削除
+                    deleteFile('tmp/tmp.wav');
+                    deleteFile('tmp/output.wav');
+                 console.log("end");
+
+                });
+            }
+            videoTrimming(starttime,duration);
+        }else{
             async function videoTrimming(starttime,duration){
                 //ffmpegを使い、音声ファイルを切り抜き
                 await childProcess.execSync(`ffmpeg -nostdin -ss ${starttime} -i tmp/tmp.mp4 -t ${duration} -c copy tmp/output.mp4`);
@@ -62,28 +87,10 @@ app.post("/download",upload.single('file'),(req,res,next)=>{
                     deleteFile('tmp/tmp.mp4');
                     deleteFile('tmp/output.mp4');
                     console.log("end");
-
-                });
-            }
-            videoTrimming(starttime,duration);
-
-        }else{
-            async function videoTrimming(starttime,duration){
-                //ffmpegを使い、音声ファイルを切り抜き
-                await childProcess.execSync(`ffmpeg -nostdin -ss ${starttime} -i tmp/tmp.wav -t ${duration} -c copy tmp/output.wav`);
-                res.download("tmp/output.wav",(err)=>{
-                    //使い終わった音声ファイルを削除
-                    deleteFile('tmp/tmp.wav');
-                    deleteFile('tmp/output.wav');
-                    console.log("end");
-
                 });
             }
             videoTrimming(starttime,duration);
         }
-    } catch(err){
-        return res.status(400).json({ err: err.array() });
-    }
 });
 
 function deleteFile(filePath){
